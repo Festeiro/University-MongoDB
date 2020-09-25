cbabc8fa19a81f69616ab3b5a873f57c383c39c3
Marcelo Schena <schenamarcelo@gmail.com>
2020-09-25 00:23:25 -0300

    Singleton

--- src/main/java/com/example/myapp/factory/DatabaseConnection.java
+++ src/main/java/com/example/myapp/factory/DatabaseConnection.java
@@ -1,33 +1,18 @@
 package com.example.myapp.factory;
 
-import java.util.List;
-import java.util.Set;
-
-import com.mongodb.DB;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 
 public class DatabaseConnection {
  
-	public static void createConnection() {
-		MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
-		 
-		@SuppressWarnings("deprecation")
-		List<String> databases = mongoClient.getDatabaseNames();
-		 
-		for (String dbName : databases) {
-		    System.out.println("- Database: " + dbName);
-		     
-		    @SuppressWarnings("deprecation")
-			DB db = mongoClient.getDB(dbName);
-		     
-		    Set<String> collections = db.getCollectionNames();
-		    for (String colName : collections) {
-		        System.out.println("\t + Collection: " + colName);
-		    }
-		}
-		 
-		mongoClient.close();
-         
-    }
+	
+	 private static MongoClient mClient;
+	 
+	    public static MongoClient getConnection() {
+	        if (mClient == null) {
+	            mClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+	        }
+	        return mClient;
+	    }
+	 
 }
